16d9df7beb1f936d1e7626c25770ef9c3010893a
Eckhart Köppen <eck@40hz.org>
2014-07-04 21:49:15 +0300

    FROM_HEX macro

--- utils.h
+++ utils.h
@@ -36,5 +36,6 @@ void hex_dump_short(uint16_t *start, int count, const char *title = 0)
 #define ARRAY_COUNT(a) (sizeof(a) / sizeof(a[0]))
 #define ABS_DIFF(a, b) (a > b ? a - b : b - a)
 #define hex_dump_periph(OUTPUT, PERIPH) hex_dump_words<OUTPUT>((uint16_t *) PERIPH, sizeof(*PERIPH), #PERIPH)
+#define FROM_HEX(c) (c >= '0' && c <= '9' ? c - '0' : (c >= 'A' && c <= 'F' ? c - 'A' + 10 : (c >= 'a' && c <= 'f' ? c - 'a' + 10 : 0)))
 
 #endif
