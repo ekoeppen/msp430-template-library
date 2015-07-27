9d3c1dbe68798d32327b0a910c9c3921d90a790d
Eckhart Köppen <eck@40hz.org>
2015-07-27 09:43:45 +0300

    Fix CC1101 example to start RX and use timeouts

--- examples/cc110x/receiver.cpp
+++ examples/cc110x/receiver.cpp
@@ -23,9 +23,10 @@ int main(void)
 	SPI::init();
 	CC1101::reset();
 	CC1101::init(cc110x_default_init_values, ARRAY_COUNT(cc110x_default_init_values));
+	CC1101::start_rx();
 	LED_RED::set_low();
 	while (1) {
-		CC1101::rx_buffer(packet, sizeof(packet));
+		CC1101::rx_buffer<TIMEOUT_NEVER>(packet, sizeof(packet));
 		LED_RED::set_high();
 		TIMEOUT::set_and_wait(50);
 		LED_RED::set_low();
